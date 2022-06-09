fbaaaf65a647336ea5c18ed5194149a590103cb2
sqwei <m18610844053@163.com>
2022-06-09 13:53:52 +0800

    do not need zygisk

--- zipthismodule.sh
+++ zipthismodule.sh
@@ -1,1 +1,1 @@
-zip -r magisk-sougoupinyin-10.32-"$1".zip * -x ./.git/**\* -x zipthismodule.sh -x README.md -x ./zygisk/**\*
+zip -r magisk-sougoupinyin-10.32-"$1".zip * -x ./.git/**\* -x zipthismodule.sh -x README.md
